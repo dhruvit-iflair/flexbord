5a685992ac019178ae825b15e7610a5583acbfae
Nasiruddin Saiyed <nasiruddin.saiyed@iflair.com>
2018-01-05 12:36:47 +0530

    Organizer Classification

--- Client/Admin/src/app/views/organizer/organizer-classifications/organizer-classifications.component.ts
+++ Client/Admin/src/app/views/organizer/organizer-classifications/organizer-classifications.component.ts
@@ -1,4 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import { environment } from "../../../../environments/environment";
+import { Router ,ActivatedRoute} from "@angular/router";
+import { ToastrService } from 'ngx-toastr';
+import { Http } from "@angular/http";
+import { HttpObserve } from '@angular/common/http/src/client';
 
 @Component({
   selector: 'app-organizer-classifications',
@@ -7,13 +12,35 @@ import { Component, OnInit } from '@angular/core';
 })
 export class OrganizerClassificationsComponent implements OnInit {
   dtOptions: DataTables.Settings = {};
-
-  constructor() { }
+  public rows :Array<any>;
+  constructor(private http : Http, private toastr : ToastrService, private router: Router,public activeRouter:ActivatedRoute) { }
 
   ngOnInit(): void {
     this.dtOptions = {
       pagingType: 'full_numbers'
     };
-  }
+    this.http.get(environment.api+'/organizerClassifications')
+              .subscribe((res)=>{
+                this.rows = res.json();
+              },(error)=>{
+              this.toastr.error('Error!! Something went wrong! try again later', 'Error');
+            });  
+    }
+    delClas(id){
+      var del = confirm("Confirm to delete this Season!");
+      if (del) {
+        this.http.delete(environment.api +"/organizerClassifications/"+id)
+                .subscribe((res)=>{
+                  var d = res.json();
+                  if (d._id) {
+                    this.toastr.success('Classifications Deleted Successfully', 'Success');
+                    // this.router.navigate(['/organizer']);
+                    this.ngOnInit();
+                  }
+                },(error)=>{
+                  this.toastr.error('Something went wrong !! Please try again later', 'Error');
+                }) 
+      }
+    }
 
 }
